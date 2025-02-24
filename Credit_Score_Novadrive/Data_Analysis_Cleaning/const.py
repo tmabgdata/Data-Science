3e6848255a2c2b37de8da5208dc896727c672fa6
Thiago Alves <tma.bigdata@gmail.com>
2025-02-24 14:19:13 -0300

    Update Repository

--- Credit_Score_Novadrive/Data_Analysis_Cleaning/const.py
+++
@@ -1,4 +0,0 @@
-# Lendo a consulta SQL do arquivo
-sql_query = ""
-with open(r"D:\Repositories\Data-Science\Credit_Score_Novadrive\queries\data_denormalization.sql", "r", encoding="utf-8") as file:
-    sql_query = file.read()
--- Credit_Score_Novadrive/queries/tables_analysis.sql
+++
@@ -1,7 +0,0 @@
-select * from clientes limit 1;
-select count(1) from clientes;
-select * from pedidocredito limit 1;
-select * from pedidocredito limit 200;
-select count(1) from pedidocredito where status = 'Aprovado';
-select * from parcelascredito limit 50;
-select * from produtosfinanciados;
